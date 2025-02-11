1a35014c5e363cfc12a5e60626c034cfaa40bad9
timmumetsa <timmu.metsa@vikk.ee>
2025-02-11 15:06:57 +0200

    Täna aitab 11.02

--- Kalkulaator_timmu_Metsa.py
+++ Kalkulaator_timmu_Metsa.py
@@ -1,7 +1,7 @@
-class cal():                            
-    def __init__(self,a,b):             
-        self.a = a
-        self.b = b
+class Calc:
+    def __init__(self, num1, num2):
+        self.a = num1
+        self.b = num2
 
     def liitmine(self):                
         return self.a + self.b
@@ -13,15 +13,15 @@ class cal():
         return self.a / self.b
     def jaak(self):                     
         return self.a % self.b
-    def ruutjuur(self):                 
+    def aste(self):
         return self.a ** self.b
 a = int(input("Sisesta esimene number: "))          
 b = int(input("Sisesta teine number: "))
 
-kalk = cal(a,b)                         
+kalk = Calc(a, b)
 while True:
     def menu():
-        x = ('1. Liitmine \n2. lahutamine\n3. korrutamine\n4. jagamine\n5. Jäägi leidmine\n6. Ruutjuure leidmine. ')
+        x = '1. Liitmine \n2. lahutamine\n3. korrutamine\n4. jagamine\n5. Jäägi leidmine\n6. Astendamine. '
         print(x)
     menu()
     valik = int(input('Sisesta üks valikutest: '))
@@ -41,7 +41,7 @@ while True:
         print("Vastus: ",kalk.jaak())
         break
     elif valik == 6:
-        print("Vastus: ",kalk.ruutjuur())
+        print("Vastus: ",kalk.aste())
         break
     elif valik == 0:
         print('Sisesta uuesti üks liitmise operaator')
