bec888ba9b40ed378749af7a30aa2ad0f22b9311
timmumetsa <timmu.metsa@vikk.ee>
2025-03-05 10:31:26 +0200

    testddd

--- Kalkulaator_timmu_Metsa.py
+++ Kalkulaator_timmu_Metsa.py
@@ -1,3 +1,5 @@
+import math  # Impordib kõik matemaatikaga seonduvad funktsioonid.
+
 class Calc:
     def __init__(self, num1, num2):
         self.a = num1
@@ -5,47 +7,87 @@ class Calc:
 
     def liitmine(self):                
         return self.a + self.b
+
     def lahutamine(self):               
         return self.a - self.b
+
     def korrutamine(self):              
         return self.a * self.b
-    def jagamine(self):                 
-        return self.a / self.b
-    def jaak(self):                     
-        return self.a % self.b
-    def aste(self):
-        return self.a ** self.b
-a = int(input("Sisesta esimene number: "))          
-b = int(input("Sisesta teine number: "))
 
-kalk = Calc(a, b)
-while True:
-    def menu():
-        x = '1. Liitmine \n2. lahutamine\n3. korrutamine\n4. jagamine\n5. Jäägi leidmine\n6. Astendamine. '
-        print(x)
-    menu()
-    valik = int(input('Sisesta üks valikutest: '))
-    if valik == 1:
-        print("Vastus: ",kalk.liitmine())
-        break
-    elif valik == 2:
-        print("Vastus: ",kalk.lahutamine())
-        break
-    elif valik == 3:
-        print("Vastus: ",kalk.korrutamine())
-        break
-    elif valik == 4:
-        print("Vastus: ",kalk.jagamine())
-        break
-    elif valik == 5:
-        print("Vastus: ",kalk.jaak())
-        break
-    elif valik == 6:
-        print("Vastus: ",kalk.aste())
-        break
-    elif valik == 0:
-        print('Sisesta uuesti üks liitmise operaator')
-        break
+    def jagamine(self): #
+        if self.b != 0: # Kontroll jagamise nulliga vältimiseks.
+            return self.a / self.b
+        else:
+            return "Viga: Nulliga jamine ei ole lubatud!" # Kuvab veateate, kui jagatakse nulliga.
+
+    def jaak(self):
+        if self.b != 0: # Kontroll nulliga jäägi vältimiseks.
+            return self.a % self.b
+        else:
+            return "viga: Nulliga jäägi leidmine ei ole lubatud!"
+
+    def aste(self): # Astendamine
+        return self.a ** self.b
+
+    def keskmine(self): # Lisatud keskmise leidmine.
+        return (self.a + self.b) / 2
+
+    def maksimum(self): # Lisatud maksimum väärtuse leidmine.
+        return max(self.a, self.b)
+
+def main(): # Kasutatud main() funktsiooni,et vältida globaalset koodi.
+    try:
+        a = int(input("Sisesta esimene number: "))
+        b = int(input("Sisesta teine number: "))
+    except ValueError: # Sisendi valideerimine, et kasutaja sisestaks ainult täisarvud.
+        print("Viga: Palun sisesta ainult täisarvud!")
+        return
+
+    kalk = Calc(a, b) # test
+
+    while True:
+        print("\nValikud:")
+        print("1. Liitmine")
+        print("2. Lahutamine")
+        print("3. Korrutamine")
+        print("4. Jagamine")
+        print("5. Jäägi Leidmine")
+        print("6. Astendamine")
+        print("7. Keskmise leidmine")
+        print("8. Maksimumväärtuse Leidmine")
+        print("9. Välju")
+   # Lisatud selged valikud ja võimalus programmi lõpetamiseks.
+
+        try:
+            valik = int(input("Sisesta üks valikutest: "))
+        except ValueError:
+            print("Viga: Palun sisesta number vahemikus 1-9!") # Kui sisestatakse vale valik kuvatakse viga.
+            continue
+
+        if valik == 1:
+            print("Vastus: ", kalk.liitmine())
+        elif valik == 2:
+            print("Vastus: ", kalk.lahutamine())
+        elif valik == 3:
+            print("Vastus: ", kalk.korrutamine())
+        elif valik == 4:
+            print("Vastus: ", kalk.jagamine())
+        elif valik == 5:
+            print("Vastus: ", kalk.jaak())
+        elif valik == 6:
+            print("Vastus: ", kalk.aste())
+        elif valik == 7:
+            print("Vastus: ", kalk.keskmine())
+        elif valik == 8:
+            print("Vastus: ", kalk.maksimum())
+        elif valik == 9:
+            print('Programmi lõpetamine.')
+            break
+        else:
+            print("Viga: Paliun vali number 1-9!")
+
+if __name__ == "__main__":
+    main()
 
 
 
